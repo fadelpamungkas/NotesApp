43135308362a5b388ee34d25f6d3f65757b22359
Del <fadelpamungkas3@gmail.com>
2018-07-16 11:13:02 +0700

    NotesApp

--- app/src/main/java/db/DatabaseContract.java
+++ app/src/main/java/db/DatabaseContract.java
@@ -1,13 +1,34 @@
 package db;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class DatabaseContract {
-    static String TABLE_NOTE = "note";
+    public static String TABLE_NOTE = "note";
 
-    static final class NoteColumns implements BaseColumns{
-        static String TITLE = "title";
-        static String DESCRIPTION = "description";
-        static String DATE = "date";
+    public static final class NoteColumns implements BaseColumns{
+        public static String TITLE = "title";
+        public String DESCRIPTION = "description";
+        public static String DATE = "date";
+    }
+
+    public static final String AUTHORITY = "com.del.mynotesapp";
+
+    public static final Uri CONTENT_URI =  new Uri.Builder().scheme("content")
+            .authority(AUTHORITY)
+            .appendPath(TABLE_NOTE)
+            .build();
+
+    public static String getColumnString(Cursor cursor, String columnName){
+        return cursor.getString(cursor.getColumnIndex(columnName));
+    }
+
+    public static int getColumnInt(Cursor cursor, String columnName){
+        return cursor.getInt(cursor.getColumnIndex(columnName));
+    }
+
+    public static long getColumnLong(Cursor cursor, String columnName){
+        return cursor.getLong(cursor.getColumnIndex(columnName));
     }
 }
